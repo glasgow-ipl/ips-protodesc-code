1b36a6696bc0d69077eadd0606cd6f59df63c970
Stephen McQuistin <sm@smcquistin.uk>
2018-12-28 22:54:33 +0000

    Add enum support

--- formatters/c_parser.py
+++ formatters/c_parser.py
@@ -182,8 +182,12 @@ size_t read_bits(BitBuffer *bbuf, uint8_t *dest, size_t num_bits) {
 		self.type_defs.append(typedef)
 		
 	def array(self, name, type, length):
-		#TODO
-		self.type_defs.append("array %s" % (name))
+		if length is None:
+			length = ""
+		typedef = """typedef struct %s {
+	%s value[%s];
+} %s;""" % (name, type, str(length), name)
+		self.type_defs.append(typedef)
 
 	def struct(self, name, fields, constraints):
 		# get field definitions
@@ -239,8 +243,22 @@ size_t read_bits(BitBuffer *bbuf, uint8_t *dest, size_t num_bits) {
 		self.parse_funcs.append(parser_func)
 		
 	def enum(self, name, variants):
-		#TODO
-		self.type_defs.append("enum %s %s" % (name, str(variants)))
+		typedef = """typedef enum %s {%s} %s;""" % (name, ", ".join("%s_%s" % (name.upper(), variant["type"].upper()) for variant in variants), name)
+		
+		funcs = []
+		for variant in variants:
+			func = """if (parse_%s(bbuf, len, parsed_%s)) {
+		return %s_%s;
+	}""" % (variant["type"].lower(), name.lower(), name.upper(), variant["type"].upper())
+			funcs.append(func)
+
+		parser_func = """int parse_%s(BitBuffer *bbuf, size_t len, %s **parsed_%s) {
+	%s
+	return -1;
+}""" % (name.lower(), name, name.lower(), "\n\telse ".join(funcs))
+
+		self.type_defs.append(typedef)
+		self.parse_funcs.append(parser_func)
 		
 	def protocol(self, name, pdus):
 		pdu_parsers = []
