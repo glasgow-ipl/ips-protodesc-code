d978e91fa72af2994c472a38eb6f2a8f70eb032a
David Southgate <d@davidsouthgate.co.uk>
2019-02-22 23:18:46 +0000

    Cleaned up old code from extended_diagrams

--- input_parsers/extended_diagrams/__init__.py
+++ input_parsers/extended_diagrams/__init__.py
@@ -1,7 +1,6 @@
 from .extended_diagrams import ExtendedDiagrams
 from .parse import Parse
-from .dictionarable import Dictionarable
 from .rel_loc import RelLoc
 from .names import Names
 
-__all__ = ["ExtendedDiagrams", "Parse", "Dictionarable", "RelLoc", "Names"]
+__all__ = ["ExtendedDiagrams", "Parse", "RelLoc", "Names"]
