e54f79dd8d123505b67b81d5aff0b5fa10ef46f7
Colin Perkins <csp@csperkins.org>
2019-05-31 19:48:00 +0100

    Parameter types may be unspecified
    Why did this typecheck?

--- protocol.py
+++ protocol.py
@@ -53,7 +53,7 @@ class ProtocolTypeError(Exception):
 @dataclass(frozen=True)
 class Parameter:
     param_name : str
-    param_type : "ProtocolType"
+    param_type : Optional["ProtocolType"]
 
 
 @dataclass(frozen=True)
