8cc8b3d164af0cb2940cc80cbc9541ba8700d633
ArcStatic <eband42@gmail.com>
2019-08-08 14:57:42 +0100

    Added revised parser file

--- generated_rust/src/main.rs
+++ generated_rust/src/main.rs
@@ -1,82 +1,75 @@
-#[macro_use]
 extern crate nom;
 
-
-enum TestEnum {
-    TypeA(u32),
-    TypeB(u32),
-}
-
+use nom::IResult; 
+use nom::{bits::bits, bits::complete::take, number::complete::be_u16, combinator::map};
+use nom::sequence::tuple;
 
 #[derive(Debug, PartialEq, Eq)]
 struct SeqNum(u16);
+
+#[derive(Debug, PartialEq, Eq)]
+struct Unused2(u8);
 
 #[derive(Debug, PartialEq, Eq)]
 struct Field6(u8);
 
 #[derive(Debug, PartialEq, Eq)]
 struct SmallStruct {
-    seq:  SeqNum,
-    f6:  Field6,
+    seq    : SeqNum,
+    f6     : Field6,
+    unused : Unused2
+}
+
+fn parse_seqnum(input : &[u8]) -> IResult<&[u8], SeqNum> {
+    map(be_u16, |x| SeqNum(x))(input)
+}
+
+fn parse_field6(input : &[u8]) -> IResult<&[u8], Field6> {
+    //map(take(6_usize), |x| Field6(x))(input)
+    map(bits::<_, _, (_, _), _, _>(take(6_usize)), |x| Field6(x))(input)
+}
+
+fn parse_unused2(input : &[u8]) -> IResult<&[u8], Unused2> {
+    //map(take(2_usize), |x| Unused2(x))(input)
+    map(bits::<_, _, (_, _), _, _>(take(2_usize)), |x| Unused2(x))(input)
+}
+
+fn parse_small_struct(input: &[u8]) -> nom::IResult<&[u8], SmallStruct>{
+    map(bits::<_, _, (_, _), _, _>(tuple((parse_seqnum, parse_field6, parse_unused2))), |(x, y, z)| SmallStruct{seq: x, f6:y, unused:z})(input)
+    //map(tuple((parse_seqnum, parse_field6, parse_unused2)), |(x, y, z)| SmallStruct{seq: x, f6:y, unused:z})(input)
 }
 
 
-#[derive(Debug, PartialEq, Eq)]
-struct Timestamp(u32);
-
-#[derive(Debug, PartialEq, Eq)]
-struct Field10(u16);
-
-#[derive(Debug, PartialEq, Eq)]
-struct TestStruct {
-    seq:  SeqNum,
-    ts:  Timestamp,
-    f6:  Field6,
-    f10:  Field10,
-    nested_struct: SmallStruct,
-}
-
-
-fn parse_SmallStruct(wire_data: &[u8]) -> nom::IResult<&[u8], SmallStruct>{
-    do_parse!(
-    wire_data,
-    parsed_data: bits!(tuple!(
-        take_bits!(16u8),
-        take_bits!(6u8)
-    )) >> (SmallStruct {
-        seq: SeqNum(parsed_data.0),
-        f6: Field6(parsed_data.1),
-    })
-)
-}
-
-
-fn parse_TestStruct(wire_data: &[u8]) -> nom::IResult<&[u8], TestStruct>{
-    do_parse!(
-    wire_data,
-    parsed_data: bits!(tuple!(
-        take_bits!(16u8),
-        take_bits!(32u8),
-        take_bits!(6u8),
-        take_bits!(10u8),
-        take_bits!(16u8),
-        take_bits!(6u8)
-    )) >> (TestStruct {
-        seq: SeqNum(parsed_data.0),
-        ts: Timestamp(parsed_data.1),
-        f6: Field6(parsed_data.2),
-        f10: Field10(parsed_data.3),
-        nested_struct: SmallStruct { seq: SeqNum(parsed_data.4), f6: Field6(parsed_data.5) },
-        })
-    )
-}
-
-//manually added for testing
 fn main() {
-    let input = [0b01000010, 0b01111111, 0b00000000, 0b00000110, 0b11001010, 0b11111101, 0b11001011, 0b01011100, 0b00000110, 0b11001010, 0b11111101, 0b11001011, 0b01011100];
-    //let input = [0b01000010, 0b01111111];
-    let res = parse_TestStruct(&input);
-    println!("{:?}", res);
-    let res = parse_SmallStruct(&input);
-    println!("{:?}", res);
+    let input = [0b01000010, 0b01111111, 0b10101010, 0b10000110, 0b11001010, 0b11111101, 0b11001011, 0b01011100, 0b00000110, 0b11001010, 0b11111101, 0b11001011, 0b01011100];
+
+    match parse_seqnum(&input) {
+        Ok((remain, result)) => {
+            println!("Remain: {:?}", remain);
+            println!("Result: {:?}", result);
+        }
+        Err(e) => {
+            println!("Failed: {:?}", e)
+        }
+    }
+
+    match parse_field6(&input) {
+        Ok((remain, result)) => {
+            println!("Remain: {:?}", remain);
+            println!("Result: {:?}", result);
+        }
+        Err(e) => {
+            println!("Failed: {:?}", e)
+        }
+    }
+
+    match parse_small_struct(&input) {
+        Ok((remain, result)) => {
+            println!("Remain: {:?}", remain);
+            println!("Result: {:?}", result);
+        }
+        Err(e) => {
+            println!("Failed: {:?}", e)
+        }
+    }
 }
