126285577b62cd77b3e648ff2463b5e78190985c
Dejice Jacob <d.jacob.1@research.gla.ac.uk>
2020-07-15 09:48:49 +0100

    tests to include directory structure

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -48,7 +48,7 @@ class Test_FileSys(ut.TestCase):
         self.assertEqual( self.rootdir, opts.root_dir,
             msg=f"Unexpected rootdir {opts.root_dir}. Expected {self.rootdir}")
 
-    def test_autogen_rootdir(self):
+    def test_default_rootdir(self):
         current = pathlib.Path.cwd()
         ap_ns, opts = npt.util.parse_cmdline(unittests="")
         self.assertIsInstance( opts.root_dir, pathlib.Path,
@@ -56,8 +56,33 @@ class Test_FileSys(ut.TestCase):
         self.assertEqual( opts.root_dir, current / "ietf_data_cache",
             msg=f"Unexpected rootdir {opts.root_dir}. Expected {self.rootdir}")
 
-    #def test_default_rootdir(self):
-    #    pass
+    def test_autogen_rootdir(self):
+        if self.rootdir.exists():
+            self.rootdir.rmdir()
+
+        ap_ns, opts = npt.util.parse_cmdline(unittests=f"-d {self.rootdir}")
+        self.assertIsInstance( opts.root_dir, pathlib.Path,
+            msg=f"generated dir - {opts.root_dir} is not of type pathlib.Path")
+        self.assertEqual( opts.root_dir, self.rootdir,
+            msg=f"Unexpected rootdir {opts.root_dir}. Expected {self.rootdir}")
+
+    def test_dir_structure(self):
+        ap_ns, opts = npt.util.parse_cmdline(unittests=f"-d {self.rootdir}")
+        with npt.util.RootWorkingDir(root=opts.root_dir) as rwd:
+            self.assertIsInstance( rwd.root, pathlib.Path,
+                msg= f"root cache dir type {type(rwd.root)} is not a pathlib.Path instance")
+            self.assertTrue(rwd.root.exists(),
+                            msg=f"Directory {rwd.root} not created")
+            self.assertTrue(rwd.root.is_dir(),
+                            msg=f"Filesys entry {rwd.root} is not a directory")
+
+            # check whether .sync file exists
+            self.assertIsInstance( rwd.sync, pathlib.Path,
+                msg= f"root cache dir type {type(rwd.root)} is not a pathlib.Path instance")
+            self.assertTrue(rwd.sync.exists(),
+                            msg=f"Sync file {rwd.sync} missing")
+            self.assertTrue(rwd.sync.is_file(),
+                            msg=f"Sync file {rwd.sync} is not a regular file")
 
     def tearDown(self):
         if self.rootdir.exists():
