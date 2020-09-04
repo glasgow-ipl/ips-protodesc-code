514c685bcf943f01bf1a906d63304cabc73020a3
Stephen McQuistin <sm@smcquistin.uk>
2020-09-04 17:28:11 +0100

    Add test cases for new example

--- examples/simple-protocol-testing/generate-pcaps.py
+++ examples/simple-protocol-testing/generate-pcaps.py
@@ -5,10 +5,20 @@ class SingleFieldHeader(Packet):
     name = "singlefieldheader"
     fields_desc=[BitField("version", 2, 8)]
 
+class MultipleFieldHeader(Packet):
+    name = "multiplefieldheader"
+    fields_desc=[BitField("version", 3, 8),
+                 BitField("field2", 10, 4),
+                 BitField("field3",  9, 4)]
 
 # Generate test pcaps
 
 # Single Field Header - valid
 cap = PcapWriter("pcaps/sfh-2-valid.pcap")
 packet = SingleFieldHeader(version=2)
+cap.write(packet)
+
+# Multiple Field Header - valid
+cap = PcapWriter("pcaps/mfh-valid.pcap")
+packet = MultipleFieldHeader()
 cap.write(packet)
--- examples/simple-protocol-testing/testharness/tests/parsing-tests.rs
+++ examples/simple-protocol-testing/testharness/tests/parsing-tests.rs
@@ -10,7 +10,57 @@ fn test_singlefieldheader_valid() {
   while let Ok(packet) = cap.next() {
       let parsed_pkt = parse_single_field_header((packet.data, 0));
       match parsed_pkt {
-          Result::Ok((_, pkt)) => { 
+          Result::Ok(((x, 0), pkt)) => {
+              assert_eq!(x, []);
+              assert_eq!(pkt.version.0, 2);
+            },
+          _ => panic!("Invalid packet")
+      }
+  }
+}
+
+#[test]
+#[should_panic]
+fn test_singlefieldheader_valid_unparsed_data() {
+  let mut cap = Capture::from_file("../pcaps/mfh-valid.pcap").unwrap();
+  while let Ok(packet) = cap.next() {
+      let parsed_pkt = parse_single_field_header((packet.data, 0));
+      match parsed_pkt {
+          Result::Ok(((x, 0), pkt)) => {
+              assert_eq!(x, []);
+              assert_eq!(pkt.version.0, 3);
+            },
+          _ => panic!("Invalid packet")
+      }
+  }
+}
+
+#[test]
+fn test_multiplefieldheader_valid() {
+  let mut cap = Capture::from_file("../pcaps/mfh-valid.pcap").unwrap();
+  while let Ok(packet) = cap.next() {
+      let parsed_pkt = parse_multiple_field_header((packet.data, 0));
+      match parsed_pkt {
+          Result::Ok(((x, 0), pkt)) => {
+              assert_eq!(x, []);
+              assert_eq!(pkt.version.0, 3);
+              assert_eq!(pkt.field2.0,  10);
+              assert_eq!(pkt.field3.0,  9);
+            },
+          _ => panic!("Invalid packet")
+      }
+  }
+}
+
+#[test]
+#[should_panic]
+fn test_multiplefieldheader_not_enough_data() {
+  let mut cap = Capture::from_file("../pcaps/sfh-2-valid.pcap").unwrap();
+  while let Ok(packet) = cap.next() {
+      let parsed_pkt = parse_multiple_field_header((packet.data, 0));
+      match parsed_pkt {
+          Result::Ok(((x, 0), pkt)) => {
+              assert_eq!(x, []);
               assert_eq!(pkt.version.0, 2);
             },
           _ => panic!("Invalid packet")
