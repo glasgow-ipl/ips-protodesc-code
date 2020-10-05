6afb75f9b3f769cdec0150e3f7b351277574debf
Stephen McQuistin <sm@smcquistin.uk>
2020-10-05 13:39:51 +0100

    Add tests for UDP parser

---
+++ tests/udp-testing/generate-pcaps.py
@@ -0,0 +1,17 @@
+from scapy.all   import *
+from scapy.utils import PcapWriter
+
+class Udp(Packet):
+    name = "udpheader"
+    fields_desc=[BitField("src_port", 33422, 16),
+                 BitField("dest_port", 53, 16),
+                 BitField("length",  13, 16),
+                 BitField("checksum", 0, 16),
+                 StrField("payload", "Hello", fmt="c")]
+
+# Generate test pcaps
+
+# UDP - valid
+cap = PcapWriter("pcaps/udp-valid-1.pcap")
+packet = Udp()
+cap.write(packet)
---
+++ tests/udp-testing/udp-testharness/tests/parsing-tests.rs
@@ -0,0 +1,26 @@
+extern crate draft_mcquistin_augmented_udp_example;
+extern crate pcap;
+
+use draft_mcquistin_augmented_udp_example::*;
+use pcap::Capture;
+
+#[test]
+fn test_parse_udp_header() {
+    let mut cap = Capture::from_file("../pcaps/udp-valid-1.pcap").unwrap();
+    while let Ok(packet) = cap.next() {
+        let mut context = Context { data_size: packet.data.len() as u32 };
+        let parsed_pkt = parse_udp_header((packet.data, 0), &mut context);
+        match parsed_pkt {
+            (Result::Ok(((x, 0), pkt)), con) => {
+                assert_eq!(con.data_size, packet.data.len() as u32);
+                assert_eq!(x, []);
+                assert_eq!(pkt.source_port.0, 33422);
+                assert_eq!(pkt.destination_port.0, 53);
+                assert_eq!(pkt.length.0, 13);
+                assert_eq!(pkt.checksum.0, 0);
+                assert_eq!(pkt.payload.0, "Hello".as_bytes());
+            },
+            _ => panic!("Invalid packet")
+        }
+    }
+}
